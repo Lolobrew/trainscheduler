db3f10fc54a4769b9d327cec3a69aad0195a09c3
Jeff Schultz <Jeff Schultz>
2017-05-21 09:50:00 -0700

    initialized files, added bootstrap, jquery, moment.js, firebase, completed intial html setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbMAVrVBP6F8IM0mID-2g7Za0ljDkg61A",
+    authDomain: "train-scheduler-ce3a0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ce3a0.firebaseio.com",
+    projectId: "train-scheduler-ce3a0",
+    storageBucket: "train-scheduler-ce3a0.appspot.com",
+    messagingSenderId: "635495989375"
+  };
+  firebase.initializeApp(config);
+
+
+  
